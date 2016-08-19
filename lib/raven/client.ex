316a2fe5f2537cc323a3930ee767c0b3607335f3
Christopher Cote <chris@entropealabs.com>
2016-08-19 14:43:01 -0500

    adds API for all supported functionality

--- lib/raven/client.ex
+++ lib/raven/client.ex
@@ -34,6 +34,62 @@ defmodule Raven.Client do
         GenServer.cast(__MODULE__, :status)
     end
 
+    def meters() do
+        GenServer.cast(__MODULE__, :meters)
+    end
+
+    def connection_status() do
+        GenServer.cast(__MODULE__, :connection_status)
+    end
+
+    def device_info() do
+        GenServer.cast(__MODULE__, :device_info)
+    end
+
+    def schedule_info() do
+        GenServer.cast(__MODULE__, :schedule_info)
+    end
+
+    def meter_info() do
+        GenServer.cast(__MODULE__, :meter_info)
+    end
+
+    def network_info() do
+        GenServer.cast(__MODULE__, :network_info)
+    end
+
+    def get_time() do
+        GenServer.cast(__MODULE__, :get_time)
+    end
+
+    def get_message() do
+        GenServer.cast(__MODULE__, :get_message)
+    end
+
+    def get_price() do
+        GenServer.cast(__MODULE__, :get_price)
+    end
+
+    def get_demand() do
+        GenServer.cast(__MODULE__, :get_demand)
+    end
+
+    def get_summation() do
+        GenServer.cast(__MODULE__, :get_summation)
+    end
+
+    def initialize() do
+        GenServer.cast(__MODULE__, :initialize)
+    end
+
+    def restart() do
+        GenServer.cast(__MODULE__, :restart)
+    end
+
+    def factory_reset() do
+        GenServer.cast(__MODULE__, :factory_reset)
+    end
+
     def init(:ok) do
         tty = Application.get_env(:raven, :tty)
         speed = Application.get_env(:raven, :speed)
@@ -48,6 +104,76 @@ defmodule Raven.Client do
 
     def handle_cast(:status, state) do
         Serial.write(Raven.Serial, Message.ConnectionStatus.command)
+        {:noreply, state}
+    end
+
+    def handle_cast(:meters, state) do
+        Serial.write(Raven.Serial, Message.MeterList.command)
+        {:noreply, state}
+    end
+
+    def handle_cast(:connection_status, state) do
+        Serial.write(Raven.Serial, Message.ConnectionStatus.command)
+        {:noreply, state}
+    end
+
+    def handle_cast(:device_info, state) do
+        Serial.write(Raven.Serial, Message.DeviceInfo.command)
+        {:noreply, state}
+    end
+
+    def handle_cast(:schedule_info, state) do
+        Serial.write(Raven.Serial, Message.ScheduleInfo.command)
+        {:noreply, state}
+    end
+
+    def handle_cast(:meter_info, state) do
+        Serial.write(Raven.Serial, Message.MeterInfo.command)
+        {:noreply, state}
+    end
+
+    def handle_cast(:network_info, state) do
+        Serial.write(Raven.Serial, Message.NetworkInfo.command)
+        {:noreply, state}
+    end
+
+    def handle_cast(:get_time, state) do
+        Serial.write(Raven.Serial, Message.TimeCluster.command)
+        {:noreply, state}
+    end
+
+    def handle_cast(:get_message, state) do
+        Serial.write(Raven.Serial, Message.MessageCluster.command)
+        {:noreply, state}
+    end
+
+    def handle_cast(:get_price, state) do
+        Serial.write(Raven.Serial, Message.PriceCluster.command)
+        {:noreply, state}
+    end
+
+    def handle_cast(:get_demand, state) do
+        Serial.write(Raven.Serial, Message.InstantaneousDemand.command)
+        {:noreply, state}
+    end
+
+    def handle_cast(:get_summation, state) do
+        Serial.write(Raven.Serial, Message.CurrentSummationDelivered.command)
+        {:noreply, state}
+    end
+
+    def handle_cast(:initialize, state) do
+        Serial.write(Raven.Serial, Message.Initialize.command)
+        {:noreply, state}
+    end
+
+    def handle_cast(:restart, state) do
+        Serial.write(Raven.Serial, Message.Restart.command)
+        {:noreply, state}
+    end
+
+    def handle_cast(:factory_reset, state) do
+        Serial.write(Raven.Serial, Message.FactoryReset.command)
         {:noreply, state}
     end
 
