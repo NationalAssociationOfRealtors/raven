f9487e8ec8b1e50277e6e5ef597a5cfffe193576
Christopher Cote <chris@entropealabs.com>
2016-08-25 13:41:50 -0500

    ignore/remove serial garbage data

--- lib/raven/client.ex
+++ lib/raven/client.ex
@@ -171,6 +171,7 @@ defmodule Raven.Client do
 
     def handle_info({:nerves_uart, _serial, data}, state) do
         IO.inspect(data)
+        Logger.debug("#{inspect String.valid?(data)}")
         message = state.message <> data |> String.trim
         {:noreply, Enum.reduce(Map.keys(@message_signatures), state, fn(tag, state) ->
             ts = tag |> Atom.to_string
