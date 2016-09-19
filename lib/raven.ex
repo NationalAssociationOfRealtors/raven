1ec0a96d7858c08025e553700e2735cbbe56ad8d
Christopher Cote <chris@entropealabs.com>
2016-09-19 13:21:11 -0500

    auto-discover UART port

--- lib/raven.ex
+++ lib/raven.ex
@@ -1,8 +1,22 @@
 defmodule Raven do
-    use Application
-    require Logger
+  use Application
+  require Logger
+  alias Nerves.UART, as: Serial
 
-    def start(_type, _args) do
-        {:ok, pid} = Raven.Supervisor.start_link
-    end
+  def start(_type, _args) do
+    get_tty
+    {:ok, pid} = Raven.Supervisor.start_link
+  end
+
+  def get_tty do
+    Serial.enumerate |> Enum.each(fn({tty, device}) ->
+      case device.product_id do
+        35368 ->
+          Logger.info("Setting Raven TTY: #{inspect tty}")
+          Application.put_env(:raven_smcd, :tty, "/dev/#{tty}", persistent: true)
+        _ -> nil
+      end
+    end)
+  end
+
 end
