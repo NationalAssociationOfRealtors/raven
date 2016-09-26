6767207048b6143e4f2a1830e59ad3aad1c5a8fc
Christopher Cote <chris@entropealabs.com>
2016-09-26 16:34:07 -0500

    fix for device discovery

--- lib/raven.ex
+++ lib/raven.ex
@@ -10,13 +10,17 @@ defmodule Raven do
 
   def get_tty do
     Serial.enumerate |> Enum.each(fn({tty, device}) ->
-      case device.product_id do
+      Logger.info("#{inspect device}")
+      case Map.get(device, :product_id, 0) do
         35368 ->
-          Logger.info("Setting Raven TTY: #{inspect tty}")
-          Application.put_env(:raven_smcd, :tty, "/dev/#{tty}", persistent: true)
+          Logger.info("Setting Meteo TTY: #{inspect tty}")
+          tty = case String.starts_with?(tty, "/dev") do
+            true -> tty
+            false -> "/dev/#{tty}"
+          end
+          Application.put_env(:raven_smcd, :tty, tty, persistent: true)
         _ -> nil
       end
     end)
   end
-
 end
